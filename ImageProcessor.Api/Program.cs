e417c21dc97797d281f08146185f9cb8e8a80f0a
Jur Wouters <jurwouters@hotmail.nl>
2026-08-16 20:30:18 +0200

    migrate on startup of api

--- ImageProcessor.Api/Program.cs
+++ ImageProcessor.Api/Program.cs
@@ -1,5 +1,7 @@
 using ImageProcessor.Api.Extensions;
+using ImageProcessor.Infrastructure.Data;
 using ImageProcessor.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,7 +23,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+    // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
